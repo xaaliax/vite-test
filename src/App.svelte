c3d442dd0910631f789e204d70ffc9dc0f5e8a86
xaaliax <nc3236pgt@students.nulondon.ac.uk>
2024-04-18 12:51:14 +0100

    changed title

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Week 14 Lab in Web Services</h1>
+  <h1>This is crazy</h1>
 
   <div class="card">
     <Counter />
